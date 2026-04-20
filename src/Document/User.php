66ab4f8e207f04771e40f2e32cd476746057b97d
phucthgch211248 <phucthgch211248@fpt.edu.vn>
2026-04-20 16:03:47 +0700

    create User document

---
+++ src/Document/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+#[ODM\Document]
+class User
+{
+    #[ODM\Id]
+    private ?string $id = null;
+
+    #[ODM\Field(type: 'string')]
+    private ?string $name = null;
+
+    #[ODM\Field(type: 'string')]
+    private ?string $email = null;
+
+    #[ODM\Field(type: 'string')]
+    private ?string $password = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function setId(?string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
